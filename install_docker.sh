c830beef8fe3b4bf0d03bbd35ce29b71a2e205fb
UnLabrador <79474439+UnLabrador@users.noreply.github.com>
2022-02-08 18:45:53 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 clear
 
+# Désinstaller les anciennes versions de docker
 apt-get remove docker docker-engine docker.io containerd runc
 
 apt-get update
 
+# Installer les dépendances
 apt-get install -y \
     ca-certificates \
     curl \
@@ -18,12 +20,15 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 apt-get update
-  
+    
+#Installer docker    
 apt-get -y install docker-ce docker-ce-cli containerd.io
 
-
+#Installer docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
 
 clear
+
+echo "Installation de docker et docker-compose terminée avec succès"
