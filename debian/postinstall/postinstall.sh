a4549252782c0a66f07862b0af92c1defc1d1e1a
UnLabrador <79474439+UnLabrador@users.noreply.github.com>
2022-02-11 20:47:58 +0100

    Update postinstall.sh

--- debian/postinstall/postinstall.sh
+++ debian/postinstall/postinstall.sh
@@ -4,3 +4,22 @@
 # Configuration automatique de Debian par Paul Pascual #
 #                                                      #
 ########################################################
+
+
+function Verif-System {
+  user=$(whoami)
+
+  if [ $(whoami) != "root" ]
+    then
+    tput setaf 5; echo "ERREUR : Veuillez exécuter le script en tant que Root !"
+    exit
+  fi
+  
+  
+  if [[ $(arch) != *"64" ]]
+    then
+    tput setaf 5; echo "ERREUR : Veuillez installer une version x64 !"
+    exit
+  fi
+  
+}
