a17a45e809e2e9f0510ca276d959460d07d5c50c
UnLabrador <79474439+UnLabrador@users.noreply.github.com>
2022-03-28 19:18:57 +0200

    Update install_zabbix_server.sh

--- debian/zabbix/zabbix-server/install_zabbix_server.sh
+++ debian/zabbix/zabbix-server/install_zabbix_server.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 clear
-# Principaux paramètres
-tput setaf 7; read -p "Entrez le mot de passe pour la base de données Zabbix : " ZABBIX_DB_USER_PASSWORD
-read -p "Entrez l'adresse ip du serveur : " SERVER_IP
-#SERVER_IP=$(hostname -i)
-tput setaf 2; echo ""
 
-apt install -y apache2 php php-mysql php-mysqlnd php-ldap php-bcmath php-mbstring php-gd php-pdo php-xml libapache2-mod-php mariadb-server mariadb-client
+apt install -y apache2 php php-mysql php-mysqlnd php-ldap php-bcmath php-mbstring php-gd php-pdo php-xml libapache2-mod-php mariadb-server mariadb-client wget
 wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian11_all.deb
 dpkg -i zabbix-release_6.0-1+debian11_all.deb
 apt update 
-apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent 
+apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent 
 
 mysql -uroot -p
 
