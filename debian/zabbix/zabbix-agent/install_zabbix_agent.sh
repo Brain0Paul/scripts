a0caf976dbb3a211047217cee57806c74d63be6d
UnLabrador <79474439+UnLabrador@users.noreply.github.com>
2022-03-28 19:18:21 +0200

    Update install_zabbix_agent.sh

--- debian/zabbix/zabbix-agent/install_zabbix_agent.sh
+++ debian/zabbix/zabbix-agent/install_zabbix_agent.sh
@@ -1,29 +1,9 @@
 #!/usr/bin/env bash
 
 clear
-tput setaf 7; read -p "Entrez le nom du serveur : " server_name
-tput setaf 7; read -p "Entrez l'ip du serveur Zabbix : " server_ip
-#tput setaf 2; dpkg -i zabbix-release_4.0-3+stretch_all.deb
+apt install wget -y
+wget https://cdn.zabbix.com/zabbix/binaries/stable/5.4/5.4.6/zabbix_agent-5.4.6-linux-4.12-ppc64le-static.tar.gz
 
-apt-get update
-apt-get install zabbix-agent -y
-
-for file in /etc/zabbix/zabbix_agentd.conf
-do
-  echo "Traitement de $file ..."
-  sed -i -e "s/Server=127.0.0.1/Server=$server_ip/g" "$file"
-  sed -i -e "s/Hostname=Zabbix server/Hostname=$server_name/g" "$file"
-done
-
-service zabbix-agent start
-systemctl enable zabbix-agent
-systemctl start zabbix-agent
-
-clear
-tput bold; tput setaf 7; echo "STATUS DU SERVICE AGENT-ZABBIX : "
-tput setaf 3; echo ""
-systemctl status zabbix-agent
-tput setaf 3; echo ""
 tput bold; tput setaf 7; echo "------------------------------------------------------------"
 tput bold; tput setaf 7; echo "             => INSTALLATION DE L'AGENT ZABBIX TERMINEE <="
 tput setaf 7; echo ""
