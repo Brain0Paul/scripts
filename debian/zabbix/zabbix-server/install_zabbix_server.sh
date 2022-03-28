0d3302240dc432bbcf9a49a7f8db13fd1162fa52
UnLabrador <79474439+UnLabrador@users.noreply.github.com>
2022-03-28 19:13:08 +0200

    Update install_zabbix_server.sh

--- debian/zabbix/zabbix-server/install_zabbix_server.sh
+++ debian/zabbix/zabbix-server/install_zabbix_server.sh
@@ -7,6 +7,13 @@ read -p "Entrez l'adresse ip du serveur : " SERVER_IP
 tput setaf 2; echo ""
 
 apt install -y apache2 php php-mysql php-mysqlnd php-ldap php-bcmath php-mbstring php-gd php-pdo php-xml libapache2-mod-php mariadb-server mariadb-client
+wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian11_all.deb
+dpkg -i zabbix-release_6.0-1+debian11_all.deb
+apt update 
+apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent 
+
+mysql -uroot -p
+
 
 tput setaf 7; echo "------------------------------------------------------------------"
 tput bold; tput setaf 7; echo "         => INSTALLATION de ZABBIX-SERVER TERMINEE <="
