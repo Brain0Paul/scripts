0a62d66630685bc873e2dde4618c4d6840865e44
Brain0Paul <79474439+Brain0Paul@users.noreply.github.com>
2022-04-15 19:02:09 +0200

    Update install_zabbix_server.sh

--- debian/zabbix/zabbix-server/install_zabbix_server.sh
+++ debian/zabbix/zabbix-server/install_zabbix_server.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 clear
 
-apt install -y apache2 php php-mysql php-mysqlnd php-ldap php-bcmath php-mbstring php-gd php-pdo php-xml libapache2-mod-php mariadb-server mariadb-client wget
+apt install -y apache2 php php-mysql php-mysqlnd php-ldap php-bcmath php-mbstring php-gd php-pdo php-xml libapache2-mod-php mariadb-server mariadb-client
+
 wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-1+debian11_all.deb
 dpkg -i zabbix-release_6.0-1+debian11_all.deb
 apt update 
-apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent 
+
+apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent 
 
 tput setaf 7; echo "------------------------------------------------------------------"
 tput bold; tput setaf 7; echo "         => INSTALLATION de ZABBIX-SERVER TERMINEE <="
