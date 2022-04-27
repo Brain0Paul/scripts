ea698c5a924e1c326ff6aca837fcdfd1a11533ec
Brain0Paul <79474439+Brain0Paul@users.noreply.github.com>
2022-04-27 11:16:02 +0200

    Update postinstall.sh

--- debian/postinstall/postinstall.sh
+++ debian/postinstall/postinstall.sh
@@ -55,6 +55,31 @@ function change-MOTD {
   
 }
 
+function Change-Password {
+  passwd root
+  tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+  tput setaf 7; echo "                                => Mot de passe de Root a été changé.                               "
+  tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+}
+
+# Changement du port SSH
+function Change-SSHPort {
+  cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+
+  for file in /etc/ssh/sshd_config
+  do
+    echo "Traitement de $file ..."
+    sed -i -e "s/#Port 22/Port $ssh_port/" "$file"
+  done  
+  tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+  tput setaf 7; echo "                                 => Port SSH remplacé par $ssh_port.                                "
+  tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+
+}
+
+
+
+
 tput setaf 6; read -p "Souhaitez vous changer le MOTD ? (y/n)  " change_motd
 if [ $change_motd = "y" ]
   then
@@ -63,3 +88,70 @@ if [ $change_motd = "y" ]
   tput setaf 6; read -p "===>     Entrez l'addresse ip du server : " ip_du_serveur
 fi
 echo ""
+
+tput setaf 6; read -p "Souhaitez vous changer le port SSH ? (recommandé) (y/n)  " change_sshport
+if [ $change_sshport = "y" ]
+  then
+    tput setaf 6; read -p "===>     Entrez port que vous souhaitez (ex : 2020) : " ssh_port
+fi
+echo ""
+
+tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+tput setaf 7; echo "                                           Début du script                                          "
+tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+echo ""
+echo ""
+
+
+tput setaf 6; echo "Vérification du système ................................................................... En cours"
+Verif-System
+tput setaf 7; echo "Vérification du système ................................................................... OK"
+echo ""
+echo ""
+
+tput setaf 6; echo "Installation des paquets essentiels........................................................ En cours"
+Install-PaquetsEssentiels
+tput setaf 7; echo "Installation des paquets essentiels........................................................ OK"
+
+echo ""
+echo ""
+if [ $create_user = "y" ]
+  then
+  tput setaf 6; echo "Création des utilisateurs et changement des mots de passe.................................. En cours"
+  Change-Password
+  tput setaf 7; echo "Création des utilisateurs et changement des mots de passe.................................. OK"
+fi
+
+echo ""
+echo ""
+if [ $change_sshport = "y" ]
+  then
+  tput setaf 6; echo "Changement du port SSH.................................................................... En cours"
+  Change-SSHPort
+  tput setaf 7; echo "Changement du port SSH.................................................................... OK"
+fi
+
+echo ""
+echo ""
+if [ $change_motd = "y" ] 
+  then
+  tput setaf 6; echo "Changement du MOTD....................................................................... En cours"
+  Change-MOTD
+  tput setaf 7; echo "Changement du MOTD....................................................................... OK"
+fi
+
+echo ""
+echo ""
+tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+tput bold; tput setaf 7; echo "                               => PREPARATION TERMINEE <=                                "
+tput setaf 7; echo ""
+tput bold; tput setaf 7; echo "                                Veuillez redémarrer votre machine                        "
+if [ $change_sshport = "y" ]
+  then
+  tput bold; tput setaf 7; echo "                             Votre nouveau port SSH : $ssh_port                        "
+fi
+tput setaf 7; echo ""
+tput bold; tput setaf 6; echo "                                       By Brain0Paul                                     "
+tput bold; tput setaf 6; echo "                                       PASCUAL.LINK                                      "
+tput setaf 7; echo "----------------------------------------------------------------------------------------------------"
+tput setaf 2; echo ""
